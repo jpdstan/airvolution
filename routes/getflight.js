481de8c7bdcaff4853f07e5e787d51150ebaefb9
fadyazmy <fadel@uwo.ca>
2017-03-18 22:08:50 +0800

    air asia api now working

--- routes/getflight.js
+++ routes/getflight.js
@@ -7,10 +7,9 @@ var http = require('http');
 var request = require('request');
 
 
-
-
 //CRON JOB for refreshing the sessions
 var CronJob = require('cron').CronJob;
+var userSession = "";
 var job = new CronJob({
     cronTime: '* 3 * * *',
     onTick: function() {
@@ -20,6 +19,7 @@ var job = new CronJob({
         request(options, function(err, res, body) {
             let json = JSON.parse(body);
             console.log(json);
+            userSession = json.data.userSession;
         });
 
     },
@@ -41,9 +41,12 @@ var options = {
 };
 
 request(options, function(err, res, body) {
-    let json = JSON.parse(body);
+    var json = JSON.parse(body);
     console.log(json);
+    userSession = encodeURIComponent(json.data.userSession);
 });
+
+
 
 
 // Play http://localhost:3000/get_flights?num_passengers=1000&arrival_station=1000&departure_date=1000&departure_station=1000=1000&return_date=1000
@@ -51,14 +54,49 @@ router.get('/*', function (req, res) {
     job.start(); // API Session updates every 4 minutes
 
     var num_passengers = req.query.num_passengers;
-    var arrival_station = req.query.arrival_station;
+    var arrival_station_POST = req.query.arrival_station;
     var departure_date = req.query.departure_date;
     var departure_station = req.query.departure_station;
     var return_date = req.query.return_date;
 
     // console.log("GET MESSAGE: "+ num_passengers + " - " + arrival_station + " - " + departure_date + " - ");
-    // Do methods here
+    // Do Flight check up here
 
+
+    //Dummy Values:
+    num_passengers = '2';
+    departure_station = 'KUL';
+    arrival_station_POST = 'SIN'; //Shanghai
+    departure_date = '2017-04-19';
+    return_date = '2017-04-20';
+
+
+    var FLIGHT_OPTIONS = {
+        url: 'https://airvolution-api.airasia.com/2017/api/',
+        method: 'POST',
+        headers: headers = {
+            'x-api-key': '15ZwK0zMir6eeiS6sPKKra98vaVWJW1i75bVqzxY',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: dataString = 'actionName=GetAvailability&adultPax=' + num_passengers +
+            '&arrivalStation=' + arrival_station_POST +
+            '&childPax=0' +
+            '&departureDate=' + departure_date +
+            '&departureStation=' +departure_station +
+            '&infantPax=0' +
+            '&returnDate=' + return_date +
+            '&userCurrencyCode=MYR' +
+            '&userSession='+ userSession
+
+    };
+
+    console.log("API REQUEST: \n "+ JSON.stringify(FLIGHT_OPTIONS));
+    request(FLIGHT_OPTIONS, function(err, res, body) {
+        let bob = JSON.parse(body);
+        console.log("API RESPONSE: \n"+JSON.stringify(bob));
+        //TODO DO SOMETHING WITH BOBBY =]
+            var response = clean_FLIGHT_RESPONSE(bob);
+    });
 
 
     res.json({ user: 'if youre seeing this kev, this may be the end...' +  num_passengers});
@@ -66,8 +104,10 @@ router.get('/*', function (req, res) {
 
 });
 
+function clean_FLIGHT_RESPONSE (res) {
+               // The function returns the product of p1 and p2
+};
+
+
 module.exports = router;
-
-
-
 
