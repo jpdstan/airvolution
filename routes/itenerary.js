c59675cd21a3fcc12787437cfb940af7b0069f7c
fadyazmy <fadel@uwo.ca>
2017-03-19 00:26:15 +0800

    itenerary clean up

--- routes/itenerary.js
+++ routes/itenerary.js
@@ -9,12 +9,13 @@ var router = express.Router();
 var http = require('http');
 var request = require('request');
 
+// Returning
 router.get('/find/:id', function (req, res, next) {
     //TODO FIND itenerary with ID
     res.send('USER')
 } )
 
-//POPULATEusing BODY (PAYLOAD)
+//POPULATE using BODY (PAYLOAD)
 router.post('/', function (req, res) {
   var secret = req.body.secret;
   res.end('Password: ' + secret);
