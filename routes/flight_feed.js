345e1921b4bfd1f75526c537acc339b72a9be5e3
Stanley Kwong <jpdstan@gmail.com>
2017-03-18 22:17:34 +0800

    process images

--- routes/flight_feed.js
+++ routes/flight_feed.js
@@ -25,46 +25,43 @@ router.get('/', function(req, res) {
     json : true
   };
 
+  function requestLikedPictures(access_token) {
+    var options = {
+      method : 'GET',
+      uri : 'https://api.instagram.com/v1/users/self/media/liked?access_token=' + access_token + "&count=1"
+    };
+
+    var process_response = function(response) {
+      res.status(200).json(response)
+    };
+
+    request(options)
+      .then(process_response)
+      .catch(function(response) {
+        res.sendStatus(400)
+      });
+  }
+
   var process_response = function(response) {
-    console.log(response);
-    res.status(200).send(response.body);
-    // requestLikedPictures(response.body.access_token);
+    console.log("Successfully processed authorized Instagram user. Response:\n" + response);
+    requestLikedPictures(response.access_token);
   };
 
   // make the request to instagram
-  request(options).then(process_response).catch(error_response);
+  request(options)
+    .then(process_response)
+    .catch(function (response) {
+      console.error(response);
+      res.sendStatus(400);
+    });
 
 });
-
-function requestLikedPictures(access_token) {
-  var options = {
-    uri : 'https://api.instagram.com/v1/users/self/media/liked?access_token=' + access_token + "&count=1",
-    method : 'GET'
-  };
-
-  var process_response = function(error, response, body) {
-    if (error) {
-
-    } else {
-      var location_list = body.data; // todo(stan): probably incorrect, hold here for now
-      for (var i = 0; i < location_list.length; i++) {
-        var location = new Location(location_list[i]);
-      }
-    }
-  };
-
-  request(options, process_response);
-}
 
 function Location(loc) {
   this.id = loc.id;
   this.latitude = loc.latitude;
   this.longitude = loc.longitude;
   this.url = loc.images.standard_resolution.url;
-}
-
-function error_response(response) {
-  console.error(response);
 }
 
 module.exports = router;
