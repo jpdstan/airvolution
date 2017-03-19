458d7c1e958f40b500bc78500ca35b865853aa8c
fadyazmy <fadel@uwo.ca>
2017-03-19 08:41:09 +0800

    better cors

--- app.js
+++ app.js
@@ -49,8 +49,9 @@ app.use(cookieParser());
 // For testing client
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods","GET, POST, PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
     next();
 });
 
